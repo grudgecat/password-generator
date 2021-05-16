94e39fcff8cb3b3cc9c2eef4d70b506ef820e022
grudgecat <grudgecat@protonmail.com>
2021-05-15 22:49:10 -0700

    added script code to create random password from selected char array data

--- Develop/script.js
+++ Develop/script.js
@@ -11,12 +11,14 @@
 //DISPLAY password to console or screen
 
 //SET CONSTANTS AND GLOBAL VARIABLES
-const alphaUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-const alphaLower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-const specialChars = ["!","@","#","$","%","^","&","*","(",")",".","?","+","-","_","=",";",":","<",">","~","{","}"];
-var password = [];
+var alphaUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var alphaLower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChars = ["!","@","#","$","%","^","&","*","(",")",".","?","+","-","_","=",";",":","<",">","~","{","}"];
+var passwdCharSet = [];
+var password = "";
 var passwdlen = 0;
+var getRandom = 0;
 var reply = "z";
 
 console.log(alphaLower);
@@ -56,9 +58,13 @@ if (reply === 'n') {
   charsets.numOfSets = charsets.numOfSets - 1;
   // charsets.numOfSets -=;
 }
+else {
+  passwdCharSet.push(...alphaUpper);
+}
 
 console.log(charsets.uc);
 console.log(charsets.numOfSets);
+console.log(passwdCharSet);
 
 //prompt user - use lower case?
 reply = "z";
@@ -71,9 +77,13 @@ while ((reply !== "y") && (reply !== "n")) {
     charsets.numOfSets = charsets.numOfSets - 1;
     // charsets.numOfSets -=;
   }
+  else {
+    passwdCharSet.push(...alphaLower);
+  }
 
 console.log(charsets.lc);
 console.log(charsets.numOfSets);
+console.log(passwdCharSet);
 
 //prompt user - use special chars?
 reply = "z";
@@ -86,9 +96,13 @@ while ((reply !== "y") && (reply !== "n")) {
     charsets.numOfSets = charsets.numOfSets - 1;
     // charsets.numOfSets -=;
   }
+  else {
+    passwdCharSet.push(...specialChars);
+  }
 
 console.log(charsets.sc);
 console.log(charsets.numOfSets);
+console.log(passwdCharSet);
 
 //prompt user - use numbers?
 reply = "z";
@@ -101,18 +115,36 @@ while ((reply !== "y") && (reply !== "n")) {
     charsets.numOfSets = charsets.numOfSets - 1;
     // charsets.numOfSets -=;
   }
+  else {
+    passwdCharSet.push(...numbers);
+  }
 
 console.log(charsets.num);
 console.log(charsets.numOfSets);
+console.log(passwdCharSet);
+
+console.log(passwdCharSet.length);
 
 if (charsets.numOfSets === 0) {
   alert("You have not selected any character sets, please try again.");
 }
 else {
-  //build array
-  //generate password
+   //generate password
   //display password
 }
+
+var i;
+for(i = 0; i < passwdlen; i++) {
+  getRandom = Math.floor((Math.random() * passwdCharSet.length));
+  // console.log(passwdCharSet[getRandom]);
+  password = password + passwdCharSet[getRandom];
+  // console.log(passwdCharSet.length);
+  // console.log(getRandom);
+}
+
+console.log(password);
+
+
 
 
 // Assignment Code
